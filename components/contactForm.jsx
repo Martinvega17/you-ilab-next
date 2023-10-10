399ed66c744096f58cada0baf910bcd60716e50c
Martin Vega Perez <martinvega0313@gmail.com>
2023-10-10 04:21:18 -0600

    upd

--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -3,7 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { useState } from "react";
-import toast, { Toaster } from 'react-hot-toast';
 import { ToastProvider, useToasts } from 'react-toast-notifications';
 
 
