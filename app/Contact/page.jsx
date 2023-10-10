92cc6f8e44ca9f81eaf22bb6ff52509eecabb434
Martin Vega Perez <martinvega0313@gmail.com>
2023-10-10 04:27:44 -0600

    il

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,15 +1,13 @@
 "use client"
 import ContactForm from "../../components/contactForm";
-import { ToastProvider } from 'react-toast-notifications';
+
 
 export default function ContactForms() {
   return (
     
-      <ToastProvider
-      position="top-center"
-  reverseOrder={false}>
+
         <ContactForm />
-      </ToastProvider>
+
     
   );
 }
--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -128,7 +128,7 @@ export default function ContactForm() {
                     <button
                         type="submit"
                         className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-buttonContact"
-                        onClick={notify}>Send message</button>
+                        >Send message</button>
                 </form>
 
 
