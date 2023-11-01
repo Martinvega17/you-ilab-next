296bf3aafcd19a9d8e48c72c7a62d9a9e14d2b8a
Martin Vega Perez <martinvega0313@gmail.com>
2023-11-01 16:09:46 -0600

    Actualizar estilo de scroll

--- components/ScrollToTopButton.jsx
+++ components/ScrollToTopButton.jsx
@@ -1,26 +1,33 @@
-"use client"
+"use client";
+import React from "react";
 import ScrollToTop from "react-scroll-to-top";
+import styled from "styled-components";
 
+const CenteredButton = styled(ScrollToTop)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #3a4f66;
+  border-radius: 50%;
+  box-shadow: none;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #FF720D;
+  }
+`;
 
 export default function ScrollToTopButton() {
-    const centerStyle = {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "transparent",
-        boxShadow: "none",
-    };
     return (
         <div className="">
-            <ScrollToTop
+            <CenteredButton
                 smooth
-                viewBox="0 0 24 24"
-                height="40px"
-                width="40px"
-                svgPath="M11 16h2v-4.2l1.6 1.6L16 12l-4-4l-4 4l1.4 1.4l1.6-1.6V16Zm1 6q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
-                color="#FEA500"
-                style={centerStyle} />
-
+                viewBox="0 0 20 20"
+                height="15px"
+                width="15px"
+                svgPath="M10,0L9.4,0.6L0.8,9.1l1.2,1.2l7.1-7.1V20h1.7V3.3l7.1,7.1l1.2-1.2l-8.5-8.5L10,0z"
+                color="#fff"
+            />
         </div>
-    )
+    );
 }
