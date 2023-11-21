bf9210e47a6ea08eb3df2eb64739880ca7f82dab
Martin Vega Perez <martinvega0313@gmail.com>
2023-11-21 02:40:27 -0600

    Update Contact page with new background image and contact information

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,33 +1,61 @@
-"use client"
 import ContactHome from "../../components/contactHome";
-
+import about from '@/assets/footer/about.webp';
 
 export default function ContactForms() {
   return (
-    <div className="flex flex-col lg:flex-row  w-full px-4 sm:px-16 py-10 mt-20">
-
-      <div className="w-full lg:w-1/2 ">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3107.4448587502206!2d-101.03606196529276!3d22.149011028114746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842a98c8d0777cff%3A0x8ed9d9a98ee5ca91!2sInstituto%20Potosino%20de%20Investigaci%C3%B3n%20Cient%C3%ADfica%20y%20Tecnol%C3%B3gica%20A.C.!5e0!3m2!1ses-419!2smx!4v1700165453297!5m2!1ses-419!2smx"
-
-          style={{ border: 0, width: "100%", height: "100%" }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade">
-
-        </iframe>
-
+    <div>
+      <div className="about">
+        <div
+          className="h-[23rem] sm:h-[20rem] md:h-[18rem] lg:h-[20rem] xl:h-[25rem] w-full flex justify-center items-center"
+          style={{
+            backgroundImage: `url(${about.src})`,
+            backgroundSize: "100%",
+          }}
+        >
+          <div className="container text-center sm:px-2 md:px-4 lg:px-8 py-4">
+            <h1 className="text-white text-3xl lg:text-6xl font-MontserratBold text-center">Contactese con el You I Lab</h1>
+          </div>
+        </div>
       </div>
 
-      <div className="w-full lg:w-1/2">
-        <ContactHome />
+      <div className="flex flex-col lg:flex-row w-full max-w-7xl mx-auto px-4 sm:px-16 py-10 mt-20 max-h-[700px]">
+        <div className="w-full lg:w-1/2">
+          {/* Información de Dirección */}
+          <h2 className="text-[#807BB1] text-5xl pb-6">Dirección</h2>
+          <p className="text-[#a5c8eb]">
+            Camino a la Presa San José 2055. <br />
+            Col. Lomas 4a sección, C.P. 78218 <br />
+            San Luis Potosí, S.L.P.
+          </p>
+
+          {/* Información de Servicio al Cliente */}
+          <h2 className="text-[#807BB1] text-5xl py-6">Servicio al Cliente</h2>
+          <p className="text-[#a5c8eb]">
+            Tel (444) 834 30 00 <br />
+            Ext. 2104 y 2108
+          </p>
+        </div>
+
+        <div className="w-full lg:w-1/2">
+          {/* Mapa de Google */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2176.234836113207!2d-101.03613648864766!3d22.149559380840252!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842a98c8d0777cff%3A0x8ed9d9a98ee5ca91!2sInstituto%20Potosino%20de%20Investigaci%C3%B3n%20Cient%C3%ADfica%20y%20Tecnol%C3%B3gica%20A.C.!5e0!3m2!1ses!2smx!4v1700555975555!5m2!1ses!2smx"
+            style={{ width: "100%", height: "100%", border: '0' }}
+            allowfullscreen=""
+            loading="lazy"
+            data-darkreader-inline-border-top=""
+            data-darkreader-inline-border-right=""
+            data-darkreader-inline-border-bottom=""
+            data-darkreader-inline-border-left=""
+          ></iframe>
+        </div>
       </div>
 
+      <div className="w-full max-w-7xl mx-auto max-h-screen pb-4">
+        <div className="w-full lg:w-full">
+          <ContactHome />
+        </div>
+      </div>
     </div>
-
-
-
-
-
-
   );
 }
