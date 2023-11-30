2bf65b8587194ab073445915c261f8e4f4a4f227
Martin Vega Perez <martinvega0313@gmail.com>
2023-11-30 02:59:18 -0600

    Add client-side import for slick styles

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,14 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const isClient = typeof window !== 'undefined';
+
+  if (isClient) {
+    // Importar y aplicar estilos solo en el lado del cliente
+    import('./styles/slick/slick.min.css');
+  }
+
+
   return (
     <html lang="es">
       <head>
@@ -26,19 +34,15 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://www.gstatic.com" />
         <link rel="preconnect" href="https://www.google.com" />
-        {/* <link rel="stylesheet" type="text/css" charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" /> */}
-          <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></Script>
- 
+
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></Script>
+
 
 
 
       </head>
 
       <body className={inter.className}>
-
         <Provider>
           <NavBar />
           {children}
